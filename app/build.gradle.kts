bab6c9e3c2909b4b8238a882c390aa55df85ce36
Eugenia Zolotova <zolotovarabota@gmail.com>
2024-05-11 23:33:42 +0300

    add all

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
+    application
     id("java")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application { mainClass.set("hexlet.code.App") }
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
